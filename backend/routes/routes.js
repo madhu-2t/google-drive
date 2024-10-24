79bf8b5dcb38689e1d6a16c9f266230d0ddb1842
madhu-2t <maruthi.tiruvaipati.21033@iitgoa.ac.in>
2024-10-24 22:34:53 +0530

    Add admin details in login_signup, getfile controller

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -8,12 +8,13 @@ const {getFolderContents}=require("../controller/getFolderContents.js");
 const {auth}=require("../middleware/auth.js")
 const {createFolder}=require("../controller/createFolder.js");
 const {uploadFile}=require("../controller/uploadFiles.js");
+const {giveFile}=require("../controller/giveFile.js");
 //Mapping controllers
 router.post("/signup",userSignup);
 router.post("/login",userLogin);
 router.get("/open/:id?",auth,getFolderContents);
 router.post("/createFolder/:id?",auth,createFolder);
 router.post("/uploadFile/:id?",auth,uploadFile);
-
+router.post("/getfile/:fileid?/:version?",auth,giveFile);
 
 module.exports=router;
